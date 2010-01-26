b0684653c7b9c8a68f22640ed747a1023d2274b7
Julian Langschaedel <meta.rb@gmail.com>
2010-01-26 02:31:18 +0100

    Split spec into own file + Add license

---
+++ spec/ffi-stree_spec.rb
@@ -0,0 +1,124 @@
+require File.join(File.dirname(__FILE__), '../lib/ffi-stree.rb')
+
+require 'bacon'
+Bacon.summary_on_exit
+
+describe Stree::Tree do
+  # deps: v4.2 (broken 4.3-pre2)
+
+  it 'initializes' do
+    @tree = Stree::Tree.new
+    @tree.entries.size.should == 0
+  end
+
+  it 'add strings to stringset' do
+    @tree << 'yokatt'
+    @tree.push 'nemukatt'
+    ->{ @tree.push 27 }.should.raise TypeError
+    @tree.entries.size.should == 2
+  end
+
+  it 'find longest common substring' do
+    @tree.longest_common_substring(0,10).should == [ 'katt' ]
+  end
+
+  it 'find longest repeated substring' do
+    @tree.entries.size.should == 2
+    @tree.longest_repeated_substring(0,0).should == [ 'katt' ]
+  end
+
+  it '#close should free objects' do
+    @tree.close
+    Stree::Active.size.should == 0
+    @tree.instance_eval { @set.should == nil; @view.should == nil }
+  end
+end
+
+
+__END__
+require 'benchmark'
+
+n = 1500
+Benchmark.bmbm do |x|
+
+  x.report("init-#{n}-2") do
+    n.times {
+      tree = Stree::Tree.new
+      tree << 'yokatt'
+      tree << 'nemukatt'
+      tree.longest_common_substring(0,10)
+      tree.close
+    } 
+  end
+
+  tree = Stree::Tree.new
+  tree << 'yokatt'
+  tree << 'nemukatt'
+
+  x.report("#{n}-2") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  n = 3000
+  x.report("#{n}-2") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  n = 6000
+  x.report("#{n}-2") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  n = n * 10
+  x.report("#{n}-2") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  tree.close
+  n = 1000
+  tree = Stree::Tree.new
+
+  100.times {
+    tree << 'yokatt'
+    tree << 'nemukatt'
+  }
+
+  x.report("#{n}-2000") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  n = n * 10
+  x.report("#{n}-2000") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  tree.close
+  n = 10
+  tree = Stree::Tree.new
+
+  10000.times {
+    tree << 'yokatt'
+    tree << 'nemukatt'
+  }
+
+  x.report("#{n}-20000") do
+    n.times {
+      tree.longest_common_substring(0,10)
+    } 
+  end
+
+  tree.close
+end
+
